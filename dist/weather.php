575eb77fea25648babe2faecd0f8b8f285aa09e5
BereniceDavid <berenice.david@hotmail.fr>
2017-05-12 16:38:52 +0200

    :sparkles: Add weather page

---
+++ dist/weather.php
@@ -0,0 +1,30 @@
+<?php
+    include 'includes/handle_form.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <title>Weather</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+    <link href="assets/css/app.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+   
+   <div class="container-fluid home">
+      <div class="row">
+            <pre class="col-md-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-xs-12">
+                 
+            </pre>
+      </div>
+    </div>
+   
+    <script src="assets/js/main.min.js"></script>
+</body>
+
+</html>
