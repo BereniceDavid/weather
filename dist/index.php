c7740058ce74f61aeefbd30336aad3b4d5c581f5
BereniceDavid <berenice.david@hotmail.fr>
2017-05-12 18:11:49 +0200

    :lipstick: Updating index & weather pages

--- dist/index.php
+++ dist/index.php
@@ -20,15 +20,15 @@
       <div class="row">
             <pre class="col-md-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-xs-12">
                 <form class="form-horizontal" action="#" method="GET">
-                    <div class="form-group text-center">
-                        <label class="col-md-12 col-sm-12 col-xs-12" for="town" class="control-label">Entrez votre ville</label>
+                    <div class="form-group form-group-lg text-center">
+                        <label class="col-md-12 col-sm-12 col-xs-12" for="town" class="control-label">Your city</label>
                         <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
                             <input type="text" class="form-control" id="town" placeholder="Paris">
                         </div>
                     </div>
                     <div class="form-group">
                         <div>
-                            <button type="submit" class="btn btn-default col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-4 col-xs-offset-4 ">Valider</button>
+                            <input type="submit" class="btn btn-default col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-4 col-xs-offset-4" value="Submit">
                         </div>
                     </div>
                 </form>    
--- dist/weather.php
+++ dist/weather.php
@@ -16,11 +16,31 @@
 
 <body>
    
-   <div class="container-fluid home">
+   <div class="container-fluid weather">
       <div class="row">
-            <pre class="col-md-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-xs-12">
-                 
-            </pre>
+            <div class="col-md-2 col-sm-3 col-xs-6 weather-card show">
+                <div class="main-infos">
+                    <div class="row">
+                        <button type="button" class="pull-right close delete-card" aria-label="Close"><span>&times;</span></button>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <div class="town">Paris</div>
+                            <div class="weather-state">Cloudy</div>
+                            <div class="temperature">20°</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="pull-right col-md-12 more-infos btn btn-info">+ Plus d'informations</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-3 col-xs-6 weather-card hidden">
+                <div class="col-md-12 text-right">
+                    <div><span>Humidity : </span><span class="humidity">20%</span></div>
+                    <div><span>Wind : </span><span class="wind">20%</span></div>
+                </div>
+            </div>
       </div>
     </div>
    
