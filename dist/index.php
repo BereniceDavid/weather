8d491daa4a4e5df2e861389b163952e567efced0
BereniceDavid <berenice.david@hotmail.fr>
2017-05-12 15:46:05 +0200

    :sparkles: Landing page

--- dist/index.php
+++ dist/index.php
@@ -7,10 +7,30 @@
     <title>Weather</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
     <link href="assets/css/app.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
    
+   <div class="container-fluid home">
+      <div class="row">
+            <pre class="col-md-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-xs-12">
+                <form class="form-horizontal" method="GET" action="#">
+                    <div class="form-group text-center">
+                        <label class="col-md-12 col-sm-12 col-xs-12" for="town" class="control-label">Entrez votre ville</label>
+                        <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                            <input type="text" class="form-control" id="town" placeholder="Paris">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div>
+                            <button type="submit" class="btn btn-default col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-4 col-xs-offset-4 ">Valider</button>
+                        </div>
+                    </div>
+                </form>    
+            </pre>
+      </div>
+    </div>
    
     <script src="assets/js/main.min.js"></script>
 </body>
